cc80e13bebfac3a1675c0010b35739fed7ceab25
guantanamobosch <luketyson17@gmail.com>
2023-03-29 14:46:20 -0600

    collector lab part 1 complete commit

--- monstercollector/main_app/urls.py
+++ monstercollector/main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('monsters/', views.monsters_index, name='index'),
 ]
--- monstercollector/main_app/views.py
+++ monstercollector/main_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+monsters = [
+    {'name': 'Aartuk Elder', 'type': 'Plant', 'size': 'Large', 'alignment': 'Typically Lawful Evil'},
+    {'name': 'Adult Blue Dracolich', 'type': 'Undead', 'size': 'Huge', 'alignment': 'Lawful Evil'},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def monsters_index(request):
+    return render(request, 'monsters/index.html', {
+        'monsters': monsters
+    })
