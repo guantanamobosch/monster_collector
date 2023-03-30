0fc67085c3a7f54e482a04c733871e176e0d6064
guantanamobosch <luketyson17@gmail.com>
2023-03-30 14:10:35 -0600

    collector lab part 3 complete - CUD functionality with CBVs

--- monstercollector/main_app/urls.py
+++ monstercollector/main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('monsters/', views.monsters_index, name='index'),
     path('monsters/<int:monster_id>', views.monsters_detail, name='detail'),
+    path('monsters/create/', views.MonsterCreate.as_view(), name='monsters_create'),
+    path('monsters/<int:pk>/update/', views.MonsterUpdate.as_view(), name='monsters_update'),
+    path('monsters/<int:pk>/delete/', views.MonsterDelete.as_view(), name='monsters_delete')
 ]
--- monstercollector/main_app/views.py
+++ monstercollector/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Monster
 
 # Create your views here.
@@ -17,3 +18,15 @@ def monsters_index(request):
 def monsters_detail(request, monster_id):
     monster = Monster.objects.get(id=monster_id)
     return render(request, 'monsters/detail.html', { 'monster': monster })
+
+class MonsterCreate(CreateView):
+    model = Monster
+    fields = '__all__'
+
+class MonsterUpdate(UpdateView):
+    model = Monster
+    fields = ['type', 'size', 'alignment', 'armor_class']
+
+class MonsterDelete(DeleteView):
+    model = Monster
+    success_url = '/monsters'
