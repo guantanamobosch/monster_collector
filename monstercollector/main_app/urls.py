7b3abfb423fe50119fb2d8db945246bfe0ed4541
guantanamobosch <luketyson17@gmail.com>
2023-04-02 19:43:09 -0600

    add dungeon habitats to monsters on the detail page

--- monstercollector/main_app/urls.py
+++ monstercollector/main_app/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('monsters/create/', views.MonsterCreate.as_view(), name='monsters_create'),
     path('monsters/<int:pk>/update/', views.MonsterUpdate.as_view(), name='monsters_update'),
     path('monsters/<int:pk>/delete/', views.MonsterDelete.as_view(), name='monsters_delete'),
-    path('monsters/<int:monster_id>/add_loot', views.add_loot, name='add_loot')
+    path('monsters/<int:monster_id>/add_loot', views.add_loot, name='add_loot'),
+    path('monsters/<int:monster_id>/assoc_dungeon/<int:dungeon_id>/', views.assoc_dungeon, name='assoc_dungeon'),
 ]
--- monstercollector/main_app/views.py
+++ monstercollector/main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Monster
+from .models import Monster, Dungeon
 from .forms import LootForm
 
 # Create your views here.
@@ -18,8 +18,10 @@ def monsters_index(request):
 
 def monsters_detail(request, monster_id):
     monster = Monster.objects.get(id=monster_id)
+    id_list = monster.dungeons.all().values_list('id')
+    uninhabited_dungeons = Dungeon.objects.exclude(id__in=id_list)
     loot_form = LootForm()
-    return render(request, 'monsters/detail.html', { 'monster': monster, 'loot_form': loot_form })
+    return render(request, 'monsters/detail.html', { 'monster': monster, 'loot_form': loot_form, 'dungeons': uninhabited_dungeons })
 
 def add_loot(request, monster_id):
     form = LootForm(request.POST)
@@ -31,7 +33,7 @@ def add_loot(request, monster_id):
 
 class MonsterCreate(CreateView):
     model = Monster
-    fields = '__all__'
+    fields = ['name', 'type', 'size', 'alignment', 'description', 'armor_class']
 
 class MonsterUpdate(UpdateView):
     model = Monster
@@ -41,3 +43,6 @@ class MonsterDelete(DeleteView):
     model = Monster
     success_url = '/monsters'
 
+def assoc_dungeon(request, monster_id, dungeon_id):
+    Monster.objects.get(id=monster_id).dungeons.add(dungeon_id)
+    return redirect('detail', monster_id=monster_id)
